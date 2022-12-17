6402e115187ce8320a06dd63cacc1e3ba426f53a
ketonBever0 <lacikurucz09@gmail.com>
2022-12-17 16:10:53 +0100

    update index.js

--- express_distances/index.js
+++ express_distances/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const fetch = require('cross-fetch');
+
 
 const run = async () => {
 
@@ -16,28 +18,35 @@ const run = async () => {
 
     app.get("/", (req, res) => res.send("Distance API"));
 
-    let distance="";
+    let distance = "";
 
 
     try {
         const response = await fetch("https://www.distance24.org/route.json?stops=Sopron|Szeged");
         distance = await response.json();
+        const stop0 = distance.stops[0].city;
+        const stop1 = distance.stops[1].city;
     }
-    catch{
-        distance={error:"Nem jó az api a gyászba vele... xd"};
+    catch {
+        distance = { error: "Nem jó az api a gyászba vele... xd" };
     }
-    
 
 
-
-
-    app.get("/distance", (req, res) => {
+    app.get("/api", (req, res) => {
         try {
-            res.json(distance)
+            res.json(distance);
         }
         catch {
             res.send(`<h1>${distance}</h2>`)
         }
+    });
+
+    app.get("/distance", (req, res) => {
+        res.json({
+            distance: distance.distance,
+            first_stop: distance.stops[0].city,
+            last_stop: distance.stops[1].city
+        })
     });
 
 
