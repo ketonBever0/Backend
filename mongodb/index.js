31f66bce441a34809f3e0f1c3c05a6b39fc7989b
ketonBever0 <lacikurucz09@gmail.com>
2023-01-11 09:51:43 +0100

    update

--- mongodb/index.js
+++ mongodb/index.js
@@ -5,11 +5,11 @@ const { errorHandler } = require('./mwares/error_middleware');
 const asyncHandler = require('express-async-handler');
 
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 
 const bcrypt = require('bcryptjs');
 
-const User = require('./models/User')
+const Users = require('./models/User')
 
 
 const app = express();
@@ -19,7 +19,9 @@ app.use(cors());
 app.use(errorHandler);
 
 
-mongoose.connect('mongodb://localhost:27017/login', () => { console.log("Connected!") }, (e) => { console.log(e) });
+mongoose.connect('mongodb://localhost:27017/login',
+    () => { console.log("Connected!") },
+    (e) => { console.log(e) });
 
 
 app.listen(8000, () => { console.log("Running!") });
@@ -29,7 +31,7 @@ app.get('/', (req, res) => res.send("MongoDB demo"));
 
 app.get('/user/:username', asyncHandler(async (req, res) => {
     const username = req.params.username;
-    const user = await User.findOne({ username: username });
+    const user = await Users.findOne({ username: username });
 
     if (!user) {
         res.status(404);
@@ -44,13 +46,13 @@ app.get('/user/:username', asyncHandler(async (req, res) => {
 
 app.post('/', asyncHandler(async (req, res) => {
     const { username, password, email, age } = req.body;
-    const user = await User.findOne({ username: username });
+    const user = await Users.findOne({ username: username });
 
     if (user) {
         res.status(400);
         throw new Error("Foglalt felhasználónév!")
     }
-    const e_mail = await User.findOne({ email: email });
+    const e_mail = await Users.findOne({ email: email });
     if (e_mail) {
         throw new Error("Foglalt e-mail cím!")
     }
@@ -62,7 +64,7 @@ app.post('/', asyncHandler(async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = await User.create({
+    const newUser = await Users.create({
         username: username,
         password: hashedPassword,
         email: email,
@@ -77,7 +79,7 @@ app.post('/', asyncHandler(async (req, res) => {
 app.post('/login', asyncHandler(async (req, res) => {
     const { username, password } = req.body;
 
-    const user = await User.findOne({ username: username });
+    const user = await Users.findOne({ username: username });
     if (!user) {
         res.status(400);
         throw new Error("Nincs ilyes felhasználó!");
@@ -89,6 +91,40 @@ app.post('/login', asyncHandler(async (req, res) => {
     }
 
     res.status(200).json(user);
+}))
+
+
+app.patch('/modosit', asyncHandler(async (req, res) => {
+    const { _id, email, age, password } = req.body;
+
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+        res.status(400);
+        throw new Error("Hibás Id");
+    }
+
+
+    const user = await Users.findById(_id);
+    if (!user) {
+        res.status(400);
+        throw new Error("Nincs ilyen felhasználó!");
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+        res.status(400);
+        throw new Error("Hibás jelszó!");
+    }
+
+    if (!email || !age) {
+        res.status(400);
+        throw new Error("Hiányos adatok!");
+    }
+
+    await Users.findByIdAndUpdate(_id, { email, age });
+
+    res.status(200);
+    res.json(await Users.findById(_id));
+
 }))
 
 
