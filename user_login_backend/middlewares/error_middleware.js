59d18acadb54dd4a2b7b03ff45f877d093459376
ketonBever0 <lacikurucz09@gmail.com>
2023-01-30 10:37:48 +0100

    moved to Web_FullStack

--- user_login_backend/middlewares/error_middleware.js
+++
@@ -1,9 +0,0 @@
-// const { rmSync } = require("fs");
-
-const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-    res.status(statusCode);
-    res.json({ message: err.message, stack: err.stack });
-}
-
-module.exports = { errorHandler };
